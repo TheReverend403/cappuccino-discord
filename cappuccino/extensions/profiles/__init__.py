9827f8c442cf9c68971501397c17c290a50ad392
Lee Watson <rev@revthefox.co.uk>
2020-09-02 19:39:55 +0100

    No need to check for a dirty session state.

--- cappuccino/extensions/profiles/__init__.py
+++ cappuccino/extensions/profiles/__init__.py
@@ -112,8 +112,6 @@ class Profiles(Extension):
 
                 if guild_member.nickname != user.nick:
                     guild_member.nickname = user.nick
-
-                if guild_member in self.db.dirty:
                     self.logger.debug(f'update_nick({user}, {user.nick}, {user.guild.id})')
             elif user.nick is not None:
                 guild_member = GuildMember(user_id=user.id, guild_id=user.guild.id, nickname=user.nick)
