45c602e746782983762633068a7842b4d218c4c6
Lee Watson <rev@revthefox.co.uk>
2020-09-04 21:17:28 +0100

    Make sure to add a trailing space to multiline strings.

--- cappuccino/extensions/catfacts/__init__.py
+++ cappuccino/extensions/catfacts/__init__.py
@@ -62,7 +62,7 @@ class Catfacts(Extension):
             self.logger.exception(exc)
             await ctx.send(
                 (
-                    "Something terrible happened while I was researching cat facts."
+                    "Something terrible happened while I was researching cat facts. "
                     "Sorry. :("
                 )
             )
